c85e1efebd7d00a89deffc29ce7753cb2d8980d3
Caoten_hoch2 <jerik.donnermeyer@gmail.com>
2024-07-04 18:42:00 +0200

    Fixed a crash

--- src/main/java/eu/caoten/adventure_map_developer/event/EntityRender.java
+++ src/main/java/eu/caoten/adventure_map_developer/event/EntityRender.java
@@ -1,5 +1,6 @@
 package eu.caoten.adventure_map_developer.event;
 
+import eu.caoten.adventure_map_developer.AdventureMapDeveloper;
 import eu.caoten.adventure_map_developer.config.ClientConfig;
 import eu.caoten.adventure_map_developer.config.api.ClientConfigValues;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
@@ -24,25 +25,29 @@ public class EntityRender implements WorldRenderEvents.AfterEntities {
         }
         MatrixStack matrices = context.matrixStack();
         IntegratedServer server = MinecraftClient.getInstance().getServer();
-        if (server.isSingleplayer()) {
-            server.getWorlds().forEach(serverWorld -> serverWorld.iterateEntities().forEach(entity -> {
-                if (entity instanceof MarkerEntity) {
-                    double x = MathHelper.lerp(context.tickCounter().getTickDelta(true), entity.lastRenderX, entity.getX()) - context.camera().getPos().getX();
-                    double y = MathHelper.lerp(context.tickCounter().getTickDelta(true), entity.lastRenderY, entity.getY()) - context.camera().getPos().getY();
-                    double z = MathHelper.lerp(context.tickCounter().getTickDelta(true), entity.lastRenderZ, entity.getZ()) - context.camera().getPos().getZ();
-                    Vec3d vec3d = MinecraftClient.getInstance().player.isInSneakingPose() ? new Vec3d(0.0, -0.125, 0.0) : Vec3d.ZERO;
-                    double d = x + vec3d.getX();
-                    double e = y + vec3d.getY();
-                    double f = z + vec3d.getZ();
-                    VertexConsumer vertexConsumer = context.consumers().getBuffer(RenderLayer.getLines());
-                    matrices.push();
-                    matrices.translate(d, e, f);
-                    matrices.translate(-vec3d.getX(), -vec3d.getY(), -vec3d.getZ());
-                    Box box = new Box(0,0,0,0.1,1,0.1);
-                    WorldRenderer.drawBox(matrices, vertexConsumer, box, 0, 128, 255, 1.0F);
-                    matrices.pop();
-                }
-            }));
+        try {
+            if (server.isSingleplayer()) {
+                server.getWorlds().forEach(serverWorld -> serverWorld.iterateEntities().forEach(entity -> {
+                    if (entity instanceof MarkerEntity) {
+                        double x = MathHelper.lerp(context.tickCounter().getTickDelta(true), entity.lastRenderX, entity.getX()) - context.camera().getPos().getX();
+                        double y = MathHelper.lerp(context.tickCounter().getTickDelta(true), entity.lastRenderY, entity.getY()) - context.camera().getPos().getY();
+                        double z = MathHelper.lerp(context.tickCounter().getTickDelta(true), entity.lastRenderZ, entity.getZ()) - context.camera().getPos().getZ();
+                        Vec3d vec3d = MinecraftClient.getInstance().player.isInSneakingPose() ? new Vec3d(0.0, -0.125, 0.0) : Vec3d.ZERO;
+                        double d = x + vec3d.getX();
+                        double e = y + vec3d.getY();
+                        double f = z + vec3d.getZ();
+                        VertexConsumer vertexConsumer = context.consumers().getBuffer(RenderLayer.getLines());
+                        matrices.push();
+                        matrices.translate(d, e, f);
+                        matrices.translate(-vec3d.getX(), -vec3d.getY(), -vec3d.getZ());
+                        Box box = new Box(0, 0, 0, 0.1, 1, 0.1);
+                        WorldRenderer.drawBox(matrices, vertexConsumer, box, 0, 128, 255, 1.0F);
+                        matrices.pop();
+                    }
+                }));
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            AdventureMapDeveloper.LOGGER.error(AdventureMapDeveloper.LOGGER_PREFIX + "Error while rendering entity: {}", e.getMessage());
         }
     }
 }
